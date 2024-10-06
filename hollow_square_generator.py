adbcda73b64501c92e337baf103a5163530bce6f
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 06:49:35 -0400

    Added a method to deal with negative input

--- hollow_square_generator.py
+++ hollow_square_generator.py
@@ -1,5 +1,9 @@
 #Generate a hollow sqaure
 def hollow_square(n):
+
+    if n <= 0:
+        raise ValueError("Invalid! The lenght cannot be negative")
+    
     for i in range(n):
         if i == 0 or i == n - 1:
             print("x" * n)
