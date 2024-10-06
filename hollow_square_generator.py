e043d4c4b596d2907db0e9151574a2677c08f724
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-10-06 06:41:48 -0400

    Added method for calculating the sides of the square

--- hollow_square_generator.py
+++ hollow_square_generator.py
@@ -1,2 +1,9 @@
 #Generate a hollow sqaure
+def hollow_square(n):
+    for i in range(n):
+        if i == 0 or i == n - 1:
+            print("x" * n)
+        else:
+            print("x" + " " * (n - 2) + "X")
+hollow_square(n)
 #make a code for getting the lenght of the sides
